a4a16a293ddc8413f369127a85fec6beec8cad83
Wuxuan <554350076@qq.com>
2023-05-22 23:09:21 +0800

    custom

---
+++ lua/custom/init.lua
@@ -0,0 +1,59 @@
+-- 换行线长度80
+vim.opt.colorcolumn = "80"
+-- 文件编码
+vim.g.encoding = "UTF-8"
+vim.o.fileencoding = 'utf-8'
+-- 行号高亮当前行
+vim.wo.cursorline = true
+-- 换行线长度为 80
+-- vim.wo.colorcolumn = "80"
+-- 缩进默认 4 个空格
+vim.o.tabstop = 4
+vim.bo.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftround = true
+-- << >> 缩进命令长度
+vim.o.shiftwidth = 4
+vim.bo.shiftwidth = 4
+-- 新行对齐当前行，空格替代tab
+vim.o.expandtab = true
+vim.bo.expandtab = true
+vim.o.autoindent = true
+vim.bo.autoindent = true
+vim.o.smartindent = true
+-- 当文件被外部程序修改时，自动加载
+vim.o.autoread = true
+vim.bo.autoread = true
+-- 禁止折行
+vim.o.wrap = false
+vim.wo.wrap = false
+-- 行结尾可以跳到下一行
+vim.o.whichwrap = 'b,s,<,>,[,],h,l'
+-- 鼠标支持
+vim.o.mouse = "a"
+-- 不可见字符的显示，这里只把空格显示为一个点
+vim.o.list = true
+vim.o.listchars = "space:·"
+-- always show tabline
+vim.o.showtabline = 2
+
+-- neovide 字体
+vim.opt.guifont = {"JetBrainsMono Nerd Font", ":h11"}
+
+-- 支持操作系统复制粘贴
+if vim.g.neovide then
+  -- vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
+  vim.keymap.set('v', '<C-Insert>', '"+y') -- Copy
+  vim.keymap.set('n', '<S-Insert>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<S-Insert>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<S-Insert>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<S-Insert>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+-- vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap('', '<S-Insert>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<S-Insert>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<S-Insert>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<S-Insert>', '<C-R>+', { noremap = true, silent = true})
+
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,18 @@
+local plugins = {
+  -- {
+  --   "vim-crystal/vim-crystal", -- 使用 vim-crystal 插件
+  --   ft = "crystal" -- 对 crystal 后缀的文件类型(fileType)生效
+  --   config = function(_) -- 启用 crystal 自动格式化
+  --       vim.g.crystal_auto_format = 1
+  --   end
+  -- }
+  -- 之后再执行 :Lazy sync 命令来让插件懒加载, 这样只有在第一次打开 crystal 文件的时候才会加载这个插件,
+  -- {
+  --  "neovim/vim-lspconfig",
+  --  config = function ()
+  --    require "plugins.configs.lspconfig"
+  --    require "custom.configs.lspconfig"
+  --  end
+  }
+}
+return plugins
